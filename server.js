27ac26f8e86a8c6a3aa36633dcc42a4a893342a4
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-01-28 13:28:43 -0300

    [crud node mongodb]
    iniciando projeto.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express =  require('express'); 
+const cookieParser = require('cookieParser');
+const cors = require('cors');
+const path = require('path');
+
+const routes = require('./src/routes');
+const { ppid } = require('process');
+
+const app = express()
+
+// views 
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json()); //formato que trocaremos as info back > front vai ser json
+app,use(routes); // caminho pra rotas
+
+app.listen(3003, function(){ // startar o server 
+    console.log("Servidor iniciado com sucesso!")
+})
