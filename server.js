a4cb49e948fd05a397308452c5d4f897b10fae62
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-01-28 16:53:55 -0300

    [conexão com o MongoDB]

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express =  require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const routes = require('./src/routes'); //utilizando as devidas rotas exportadas 
 const { ppid } = require('process');
 
 const app = express()
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/CrudNodeMongoDB', 
+async(err) => {
+    if(err) throw err;
+    console.log('connected to MongoDB')
+});
 
 // views 
 
